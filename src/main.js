6c3576e811972c83b708dde21579407b5b68b91e
chereseeriepa <cherese.eriepa@outlook.com>
2022-09-23 13:45:57 +1200

    init apollo-composable

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+// apollo
+import { DefaultApolloClient } from '@vue/apollo-composable'
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core'
+
 import App from './App.vue'
 import router from './router'
 
@@ -10,9 +14,22 @@ import quasarUserOptions from './quasar-user-options'
 
 import "quasar/dist/quasar.sass"
 
-const app = createApp(App).use(Quasar, quasarUserOptions)
+// TODO dont hardcode the port
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000/graphql'
+})
 
+const cache = new InMemoryCache()
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache
+})
+
+const app = createApp(App)
+
+app.use(Quasar, quasarUserOptions)
 app.use(createPinia())
 app.use(router)
+app.provide(DefaultApolloClient, apolloClient)
 
 app.mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,19 @@
-<script setup>
-</script>
-
 <template>
-  <div>
-    <h1>HELLO WORLD</h1>
-  </div>
+  <h2>Hello {{ result?.getProfile?.name ?? 'world' }}</h2>
 </template>
+
+<script setup>
+  import { useQuery } from '@vue/apollo-composable'
+  import gql from 'graphql-tag'
+
+  const mixId = '@DIoOBMaI1f0mJg+5tUzZ7vgzCeeHh8+zGta4pOjc+k0=.ed25519'
+ 
+  const { result } = useQuery(gql`
+    query {
+      getProfile(id: "${mixId}") {
+        id
+        name
+      }
+    }
+  `)
+</script>
