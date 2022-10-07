a150aa16d78ae1f22af485c5f54789453214bf0d
mixmix <mix@protozoa.nz>
2022-10-07 20:32:40 +1300

    add custom fetch which allows calling http API from https domain

--- src/plugins/apollo.js
+++ src/plugins/apollo.js
@@ -6,7 +6,13 @@ import {
 
 // TODO dont hardcode the port
 const httpLink = createHttpLink({
-  uri: import.meta.env.VITE_GRAPHQL_API || 'http://localhost:4000/graphql'
+  uri: import.meta.env.VITE_GRAPHQL_API || 'http://localhost:4000/graphql',
+
+  // TODO remove the is hack when add https Graphql API
+  fetch: (uri, options) => fetch(uri, {
+    ...options,
+    referrerPolicy: 'unsafe_url'
+  })
 })
 
 const cache = new InMemoryCache()
