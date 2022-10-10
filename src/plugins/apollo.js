b0a40a8d4a6194a6e432619cb5766bb557081f37
chereseeriepa <cherese.eriepa@outlook.com>
2022-10-10 19:19:14 +1300

    add cors config

--- src/plugins/apollo.js
+++ src/plugins/apollo.js
@@ -6,7 +6,10 @@ import {
 
 // TODO dont hardcode the port
 const httpLink = createHttpLink({
-  uri: import.meta.env.VITE_GRAPHQL_API || 'http://localhost:4000/graphql'
+  uri: import.meta.env.VITE_GRAPHQL_API || 'http://localhost:4000/graphql',
+  credentials: process.env.NODE_ENV === 'development'
+    ? 'same-origin' // for local graphql server
+    : 'include' // for remote graphql server
 })
 
 const cache = new InMemoryCache()
