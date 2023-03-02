d866c4344884e8b157b2fbba4e6db540aae85953
rabble <evan@protest.net>
2023-03-03 08:06:56 +1300

    extra ' was breaking everything.

--- src/plugins/posthog.js
+++ src/plugins/posthog.js
@@ -6,7 +6,7 @@ export default {
     app.config.globalProperties.$posthog = posthog.init(
       "phc_WGxyEqUxYhM1SePQGdjrxBoDgcv9qIYlkwusMFS6MK1",
       {
-        api_host: "https://app.posthog.com'",
+        api_host: "https://app.posthog.com",
       }
     );
   },
